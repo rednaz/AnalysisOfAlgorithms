0b8acd62fef02b128777166e092f6f53519ee3c2
JamesMusselman <jamesmusselman@live.com>
2016-11-07 17:30:20 -0800

    Fixed graph builder, added algorithm (not yet fully implemented)

--- SavitchAlgorithm/Savitch.py
+++ SavitchAlgorithm/Savitch.py
@@ -38,18 +38,48 @@ class Graph:
 def GraphBuilder(n):
     nodes = list()
     graph = Graph()
+    count = 0
 
-    for i in range(n):
+    for i in range(n**2): #n^2 nodes in the graph
         newNode = Node([1, n])
         nodes.append(newNode)
         graph.AddNode(newNode)
-        #print "created node..."
+        print("Created node: " + str(i))
+    print()
 
+    for x in range(n**2):
+        for y in range(n**2):
+            if ((y == (x+1)) or (y == (x-1)) or (y == (x+n)) or (y == (x-n))): #If y is adjacent to x...
+                graph.AddDirectedEdge(nodes[x], nodes[y]) #connect the two nodes.
+                #print(str(x) + " -> " + str(y)) #Uncomment this to check for correct edges
+
+    #Print graph
+    print("nxn graph with " + str(n**2) + " nodes:")
     for x in range(n):
         for y in range(n):
-            graph.AddDirectedEdge(nodes[x], nodes[y]) #directed of bidirected? Change if need be
-            print(str(x) + " -> " + str(y))
+            if (y == (n-1)):
+                print("*", end="")
+            else:
+                print("*-", end="")
+        print()
+
+        if (x != (n-1)):
+            for z in range(n):
+                print("| ", end="")
+        print()
+
     return graph
+
+#Savitch's Algorithm
+def k_edge_path(s, t, k):
+    if k == 0:
+        return s == t
+    if k == 1:
+        return s == t or (s, t) in edges
+    for u in vertices:
+        if k_edge_path(s, u, floor(k / 2)) and k_edge_path(u, t, ceil(k / 2)):
+            return true
+    return false
 
 #Main
 invalid = True
